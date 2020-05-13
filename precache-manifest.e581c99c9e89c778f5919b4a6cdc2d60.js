e39ed632c103b475734b88c3aab9fdd68f5942f9
Solacup <solacup07@naver.com>
2020-05-13 20:36:40 +0900

    Updates

---
+++ precache-manifest.e581c99c9e89c778f5919b4a6cdc2d60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c925555b15a96863e28afcbc5f1ba95c",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "5432b19b5fbe30e9d978",
+    "url": "/movie_app/static/css/main.aa53e387.chunk.css"
+  },
+  {
+    "revision": "964546851f2431c5d268",
+    "url": "/movie_app/static/js/2.2d3cfcc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.2d3cfcc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5432b19b5fbe30e9d978",
+    "url": "/movie_app/static/js/main.c4abcfb5.chunk.js"
+  },
+  {
+    "revision": "ffbd4de4896f1a599175",
+    "url": "/movie_app/static/js/runtime-main.0186e20d.js"
+  }
+]);
